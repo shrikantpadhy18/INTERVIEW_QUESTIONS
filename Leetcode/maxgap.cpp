1219906dd22fcda6fdbd9c04f76d0ab6b1d1677e
shrikant <shrikantpadhy18@gmail.com>
2020-05-27 10:45:43 +0530

    done

---
+++ Leetcode/maxgap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if(nums.size()==1){
+            return 0;
+        }
+        else{
+            sort(nums.begin(),nums.end());
+            int ans=0;
+            for(int i=1;i<nums.size();i++){
+                ans=max(ans,nums[i]-nums[i-1]);
+            }
+            return ans;
+        }
+    }
+};
---
+++ Leetcode/readme.md
@@ -0,0 +1,5 @@
+MaxGap:Given an unsorted array, find the maximum difference between the successive elements in its sorted form.
+
+Return 0 if the array contains less than 2 elements.
+<a href="https://leetcode.com/problems/maximum-gap/">PROBLEM LINK</a>
+&nbsp FINISH &nbsp
